ce55a64ccd53c010688038bfb921e9caae3fddea
Robert Odoch <robertelvisodoch@gmail.com>
2019-07-01 13:58:56 +0300

    Implement API endpoint for creating a new property

--- API/api1.js
+++ API/api1.js
@@ -3,9 +3,10 @@ const uuidv4 = require('uuid/v4');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const { getUserJson } = require('./methods');
+const { ensureToken, getTokenUsername, getUserJson } = require('./methods');
 
 let users = [];
+let properties = [];
 
 const api = express.Router();
 
@@ -84,6 +85,69 @@ api.post('/auth/signin', (req, res) => {
     });
 
     return;
+});
+
+// All routes after this point requires user to be logged in.
+api.use(ensureToken);
+
+api.post('/property', (req, res) => {
+    const property = req.body;
+    if (property.name === '' || property.type === '' || property.picture === '' ||
+            property.price === '' || property.status === '' || property.state === '' ||
+            property.city === '' || property.address === '') {
+
+        res.status(400).json({
+            'status': 'error',
+            'message': 'Incomplete details submitted!'
+        });
+
+        return;
+    }
+
+    // Get the user posting the property
+    const token = req.headers['x-access-token'];
+    const username = getTokenUsername(token);
+    if (!username) {
+        res.status(500).json({
+            'status': 'error',
+            'error': 'Something went wrong'
+        });
+
+        return;
+    }
+
+    const user = users.find((u) => {
+        return u.username === username;
+    });
+    if (!user) {
+        res.status(500).json({
+            'status': 'error',
+            'error': 'Something went wrong!'
+        });
+
+        return;
+    }
+
+    // Store the new property.
+    const newProperty = {
+        'id': uuidv4(),
+        'owner': user.id,
+        'name': property.name,
+        'status': property.status,
+        'type': property.type,
+        'state': property.state,
+        'city': property.city,
+        'address': property.address,
+        'price': property.price,
+        'created_on': new Date(),
+        'image_url': 'http://www.example.com'
+    };
+    properties.push(newProperty);
+
+    res.status(201).json({
+        'status': 'success',
+        'data': newProperty
+    });
 });
 
 module.exports = api;
