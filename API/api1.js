16352f5513f4265409b79c7651fd9c9ba766b286
Robert Odoch <robertelvisodoch@gmail.com>
2019-07-02 17:13:56 +0300

    Improve validation logic

--- API/api1.js
+++ API/api1.js
@@ -12,9 +12,9 @@ const api = express.Router();
 
 api.post('/auth/signup', (req, res) => {
     const user = req.body;
-    if (user.firstName === '' || user.lastName === '' || user.email === '' ||
-            user.phoneNumber === '' || user.address === '' || user.username === '' ||
-            user.password === '' || user.passwordConfirmation === '') {
+    if (!user.firstName || !user.lastName || !user.email ||
+            !user.phoneNumber || !user.address || !user.username ||
+            !user.password || !user.passwordConfirmation) {
         res.status(400).json({
             "status": "error",
             "error": "Some details are missing"
@@ -92,9 +92,9 @@ api.use(ensureToken);
 
 api.post('/property', (req, res) => {
     const property = req.body;
-    if (property.name === '' || property.type === '' || property.picture === '' ||
-            property.price === '' || property.status === '' || property.state === '' ||
-            property.city === '' || property.address === '') {
+    if (!property.name || !property.type || !property.picture ||
+            !property.price || !property.status || !property.state ||
+            !property.city || !property.address) {
 
         res.status(400).json({
             'status': 'error',
